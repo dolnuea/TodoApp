5b77e9a98b46e98f0eef2e5a5dee24c9620c20a7
Luna Dagci <ldagci@albany.edu>
2021-12-23 14:41:14 -0500

    Rendering Items

--- app/src/main/java/com/luna/simpletodo/MainActivity.kt
+++ app/src/main/java/com/luna/simpletodo/MainActivity.kt
@@ -2,10 +2,47 @@ package com.luna.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val listOfTasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listOfTasks.add("meow")
+
+        // Lookup the recyclerview in activity layout
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        // Create adapter passing in the sample user data
+        val adapter = TaskItemAdapter(listOfTasks)
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.adapter = adapter
+        // Set layout manager to position the items
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    /**
+     * Add
+     */
+    fun addTask(savedInstanceState: Bundle?){
+        //todo
+    }
+
+    /**
+     * Remove
+     */
+    fun removeTask(){
+        //todo
+    }
+
+    /**
+     * Edit
+     */
+    fun editTask(){
+
     }
 }
---
+++ app/src/main/java/com/luna/simpletodo/TaskItemAdapter.kt
@@ -0,0 +1,47 @@
+package com.luna.simpletodo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * bridge that displays data given on recycleview
+ */
+class TaskItemAdapter (private val listOfItems: List<String>) : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val textView : TextView
+
+        init {
+            textView = itemView.findViewById(android.R.id.text1)
+        }
+    }
+
+    // ... constructor and member variables
+    // Usually involves inflating a layout from XML and returning the holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    // Returns the total count of items in the list
+    override fun getItemCount(): Int {
+        return listOfItems.size
+    }
+
+    // Involves populating data into the item through holder
+    override fun onBindViewHolder(holder: TaskItemAdapter.ViewHolder, position: Int) {
+
+        //get data model based on pos
+        val item = listOfItems.get(position)
+
+        holder.textView.text = item
+    }
+
+}
